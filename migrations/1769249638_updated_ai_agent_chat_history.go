121d6a837f6a29c2574a50759beb18ee5ca3611f
Viet Pham <viet@webonyx.com>
2026-01-24 17:14:38 +0700

    increase chat_audio file size

---
+++ migrations/1769249638_updated_ai_agent_chat_history.go
@@ -0,0 +1,64 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_333196930")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"hidden": false,
+			"id": "file410859157",
+			"maxSelect": 1,
+			"maxSize": 20971520,
+			"mimeTypes": [
+				"audio/wav",
+				"audio/mpeg",
+				"audio/flac"
+			],
+			"name": "chat_audio",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": [],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_333196930")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"hidden": false,
+			"id": "file410859157",
+			"maxSelect": 1,
+			"maxSize": 0,
+			"mimeTypes": [],
+			"name": "chat_audio",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": [],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
